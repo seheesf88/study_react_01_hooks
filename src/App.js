038ecbf5e66ef8d1f0398d4c6139c1d74a2a6701
SeHee Son <seheeson@MacBook-Pro.local>
2020-09-09 00:54:16 -0700

    make simple count function by using hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  //useState takes two values array
+  //count is state
+  //setCount is function that can update state
+  //useState is hook method, indside of () is initial value for the state
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  }
+
   return (
-    <div>
-      It's hooks!
+    <div className="app">
+      <div>Studying Hooks</div>
+      <div className="title">Counting Number</div>
+      <div className="counting_app">
+        <button onClick={decrement}>-</button>
+        <div className="display">{count}</div>
+        <button onClick={increment}>+</button>
+      </div>
     </div>
   );
 }
